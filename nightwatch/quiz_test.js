d60bfccbc18b18a358b0676bad4220c6eede3a73
PrecieuxMudibu <oceangpmb@gmail.com>
2023-01-10 12:49:37 +0100

    Install nightwatch

--- nightwatch/quiz_test.js
+++ nightwatch/quiz_test.js
@@ -13,3 +13,22 @@ module.exports = {
             .end()
     }
 }
+
+
+// BONNE REPONSE
+// module.exports = {
+//     test: client => {
+//         client
+//             .url('http://127.0.0.1:5500/')
+//             .waitForElementVisible('.main-title', 10 * 1000)
+//             .assert.visible('input[type=email]')
+//             .setValue('input[type=email]', 'thomas@facada.com')
+//             .click('button[type=submit]')
+//             .assert.visible('.user-email-error-msg')
+//             .clearValue('input[type=email]') // LA DIFFERENCE EST ICI
+//             .setValue('input[type=email]', 'thomas@facadia.com')
+//             .click('button[type=submit]')
+//             .assert.not.visible('.user-email-error-msg')
+//             .end()
+//     }
+// }
